6803367328fc9abc30d5c78f3528916ef9db8c4c
manucarbonell <manucarbonell@github.com>
2019-11-25 16:45:59 +0100

    Training 3 task visualization

---
+++ plot_training.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = sys.argv[1]
+fin=os.path.join('trained_models',dataset+'_3in1_seq_log.txt')
+train_log= open(fin,'r')
+lines = train_log.readlines()
+cers=[]
+ner_maps=[]
+det_maps=[]
+epochs = []
+for line in lines:
+    vals = line.split(' ')
+    if len(vals)!=6: continue
+    epochs.append(int(vals[0]))
+    
+    cers.append(1-float(vals[1]))
+    ner_maps.append(float(vals[3]))
+    det_maps.append(float(vals[5]))
+    
+cers = np.array(cers)[:45]
+ner_maps=np.array(ner_maps)[:45]
+det_maps=np.array(det_maps)[:45]
+epochs = np.array(epochs)[:45]
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(epochs,det_maps, 'r',epochs,cers, 'b', epochs,ner_maps, 'g')
+plt.show()
+
